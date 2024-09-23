5bf5b0a4bf96df507eed08cf78e6cd1959a8e6df
m-hrachya <62440346+m-hrachya@users.noreply.github.com>
2024-09-23 20:54:20 +0400

    Final Project

--- implementation.c
+++ implementation.c
@@ -1,10 +1,8 @@
 #include "lib.h"
 
-extern int regs[REG_COUNT];
-extern int wsr;
-extern int rip;
-extern int simulated_memory[MEM_SIZE];
+extern mem_t memory;
 extern char command_buffer[COMMAND_SIZE];
+extern int fd;
 
 void alu_logic(int first_reg, int second_reg, int *dest_reg, int opcode)
 {
@@ -18,13 +16,13 @@ void alu_logic(int first_reg, int second_reg, int *dest_reg, int opcode)
 void layo()
 {
     printf("CPU State:\n\n");
-    printf("R0: %d, R1: %d, R2: %d, R3: %d, R4: %d, R5: %d, R6: %d, WSR: %d, IP: %d\n\n", regs[0], regs[1], regs[2], regs[3], regs[4], regs[5], regs[6], wsr, rip);
-    // printf("Memory:  [");
-    // for (int i = 0; i < MEM_SIZE; ++i)
-    // {
-    //     printf("%d ", simulated_memory[i]);
-    // }
-    // printf("\b] (size 256)\n");
+    printf("R0: %d, R1: %d, R2: %d, R3: %d, R4: %d, R5: %d, R6: %d, WSR: %d, IP: %d\n\n", memory.regs[0], memory.regs[1], memory.regs[2], memory.regs[3], memory.regs[4], memory.regs[5], memory.regs[6], memory.wsr, memory.rip);
+    printf("Memory:  [");
+    for (int i = 0; i < MEM_SIZE; ++i)
+    {
+        printf("%d ", memory.simulated_memory[i]);
+    }
+    printf("\b] (size %d)\n", MEM_SIZE);
 }
 
 void mov(int *reg, int value)
@@ -39,20 +37,21 @@ void control_unit()
     char *token = strtok(command_buffer, " ,");
     if (!strcmp(token, "START"))
     {
-        wsr = WSR_ACTIVE;
+        if (memory.wsr == WSR_DEACTIVE) ++memory.rip;
+        memory.wsr = WSR_ACTIVE;
     }
     else if (!strcmp(token, "EXIT"))
     {
-        if (wsr == WSR_DEACTIVE)
+        if (memory.wsr == WSR_DEACTIVE)
         {
+            close(fd);
             exit(EXIT_SUCCESS);
         }
-        wsr = WSR_DEACTIVE;
+        memory.wsr = WSR_DEACTIVE;
         memset(command_buffer, 0, COMMAND_SIZE);
-        memset(regs, 0, sizeof(int) * REG_COUNT);
-        rip = 0;
+        memset(memory.regs, 0, sizeof(int) * REG_COUNT);
     }
-    else if ((!strcmp(token, "ADD") || !strcmp(token, "SUB")) && (wsr == WSR_ACTIVE))
+    else if ((!strcmp(token, "ADD") || !strcmp(token, "SUB")) && (memory.wsr == WSR_ACTIVE))
     {
         int opcode;
         if (!strcmp(token, "ADD"))
@@ -69,30 +68,86 @@ void control_unit()
         if (first_reg < 0 || second_reg < 0 || dest_reg < 0 || first_reg > 6 || second_reg > 6 || dest_reg > 6)
         {
             printf("Invalid instruction!\n");
+            memset(command_buffer, 0, COMMAND_SIZE);
             return;
         }
-        rip++;
-        alu_logic(regs[first_reg], regs[second_reg], &regs[dest_reg], opcode);
+        memory.rip++;
+        alu_logic(memory.regs[first_reg], memory.regs[second_reg], &memory.regs[dest_reg], opcode);
     }
     else if (!strcmp(token, "LAYO"))
     {
-        rip++;
+        memory.rip++;
         layo();
     }
-    else if ((!strcmp(token, "MOV")) && (wsr == WSR_ACTIVE))
+    else if ((!strcmp(token, "MOV")) && (memory.wsr == WSR_ACTIVE))
     {
         int first_reg, value;
         token = strtok(NULL, " ,");
         first_reg = atoi(token + 1);
         token = strtok(NULL, " ,");
-        value = atoi(token);
+        if (*token == 'R') {
+            value = atoi(token + 1);
+            if (value < 0 || value > 6) {
+                printf("Invalid Instruction!\n");
+                memset(command_buffer, 0, COMMAND_SIZE);
+                return;
+            }
+            value = memory.regs[value];
+        } else value = atoi(token);
         if (first_reg < 0 || first_reg > 6)
         {
             printf("Invalid Instruction!\n");
+            memset(command_buffer, 0, COMMAND_SIZE);
             return;
         }
-        mov(&regs[first_reg], value);
-        rip++;
+        mov(&memory.regs[first_reg], value);
+        memory.rip++;
+    }
+    else if ((!strcmp(token, "LOAD") || !strcmp(token, "STORE")) && (memory.wsr == WSR_ACTIVE)) {
+        int is_load;
+        if (!strcmp(token, "LOAD")) is_load = 1;
+        else is_load = 0;
+        int reg_index, address;
+        token = strtok(NULL, " ,");
+        reg_index = atoi(token + 1);
+        if (reg_index < 0 || reg_index > 6) {
+                printf("Invalid Instruction!\n");
+                memset(command_buffer, 0, COMMAND_SIZE);
+                return;
+        }
+        token = strtok(NULL, " ,");
+        address = atoi(token);
+        if (address < 0 || address > MEM_SIZE) {
+            printf("Invalid Instruction!\n");
+            memset(command_buffer, 0, COMMAND_SIZE);
+            return;
+        }
+        if (is_load) {
+            memory.regs[reg_index] = memory.simulated_memory[address];
+        } else {
+            memory.simulated_memory[address] = memory.regs[reg_index];
+        }
+        ++memory.rip;
+    } else if ((!strcmp(token, "DISC") && (memory.wsr == WSR_ACTIVE))) {
+        if (memory.rip == 0) {
+            return;
+        }
+        int count = 1;
+        token = strtok(NULL, " ,");
+        if (token) {
+            count = atoi(token);
+        }
+        lseek(fd, 0, SEEK_SET);
+        int file_size = lseek(fd, 0, SEEK_END);
+        lseek(fd, 0, SEEK_SET);
+        ftruncate(fd, file_size - sizeof(mem_t) * count);
+        memory.rip -= count;
+        lseek(fd, -sizeof(mem_t), SEEK_END);
+        read(fd, &memory, sizeof(mem_t));
+        return;
+    } else {
+        printf("Invalid Instruction! Try running START first!\n");
     }
     memset(command_buffer, 0, COMMAND_SIZE);
+    write(fd, &memory, sizeof(mem_t));
 }
